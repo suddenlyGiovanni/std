e3407063d9ad50e2599c6819ef839cb8eba39d66
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-05-27 20:21:12 +0200

    docs(internal): Add method comments to Equals interface
    Expanded the documentation for the Equals interface. A method comment
     has been added to provide more detail about the equals function,
     including its usage and parameters.

--- src/internal/equals.ts
+++ src/internal/equals.ts
@@ -2,5 +2,9 @@
  * An interface containing operations for equality.
  */
 export interface Equals {
+	/**
+	 * Checks if `this` value is equal to`that`  value.
+	 * @param that - The value to compare to.
+	 */
 	equals(that: unknown): boolean
 }
