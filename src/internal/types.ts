ffea28ec8c20810ee2f8d4cc9e1dfac370c12e07
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-05-28 21:46:50 +0200

    types(internal): Add 'types.ts' for utilities handling TypeScript types
    This commit introduces a new 'types.ts' file in the 'src/internal'
    directory. This file contains TypeScript utility types such as
    'Tags', 'Equals', and 'Simplify', each complete with examples and
    documentation. These utilities are designed to help handling
    TypeScript types much easier in the project.

---
+++ src/internal/types.ts
@@ -0,0 +1,49 @@
+export declare namespace Types {
+	/**
+	 * Returns the tags in a type.
+	 *
+	 *  @example
+	 *```ts
+	 * import type { Types } from './types'
+	 *
+	 * type Res = Types.Tags<string | { _tag: 'a' } | { _tag: 'b'} > // "a" | "b"
+	 *```
+	 *
+	 * @category types
+	 */
+	export type Tags<E> = E extends { _tag: string } ? E['_tag'] : never
+
+	/**
+	 * Determines if two types are equal.
+	 *
+	 * @example
+	 * ```ts
+	 * import type { Types } from './types'
+	 *
+	 * type Res1 = Types.Equals<{ a: number }, { a: number }> // true
+	 * type Res2 = Types.Equals<{ a: number }, { b: number }> // false
+	 *```
+	 *
+	 * @category models
+	 */
+	export type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2
+		? true
+		: false
+
+	/**
+	 * Simplifies the type signature of a type.
+	 *
+	 * @example
+	 *```ts
+	 * import type { Types } from './types'
+	 *
+	 * type Res = Types.Simplify<{ a: number } & { b: number }> // { a: number; b: number; }
+	 * ```
+	 * @category types
+	 */
+	export type Simplify<A> = {
+		[K in keyof A]: A[K]
+	} extends infer B
+		? B
+		: never
+}
