2e45f78a1485fa6ce635b6a1569a3de2b80a252b
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-06-07 19:03:40 +0200

    docs(internal): Add detailed documentation to typeclass module
    A comprehensive explanation has been added to the typeclass module,
    providing a clear and concise description of its functionality and
    sophistication. This includes a diagram representing the
    parameterized types hierarchy, as well as credits to the original
    authors. These changes enhance the maintainability and comprehension
    of this crucial segment of the codebase.

--- src/typeclass/mod.ts
+++ src/typeclass/mod.ts
@@ -1,3 +1,39 @@
+/**
+ * This internal module provides a collection of re-usable type classes adapted from the `Scala's Cats` library and [`@effect/typeclass`](https://github.com/Effect-TS/effect/blob/184fed83ac36cba05a75a5a8013f740f9f696e3b/packages/typeclass/README.md).
+ *
+ * [!IMPORTANT]
+ * Credits to the original authors of the Scala Cats library and `@effect/typeclass`.
+ *
+ * **Parameterized Types Hierarchy**
+ *
+ * ```mermaid
+ * flowchart TD
+ *     Alternative --> SemiAlternative
+ *     Alternative --> Coproduct
+ *     Applicative --> Product
+ *     Coproduct --> SemiCoproduct
+ *     SemiAlternative --> Covariant
+ *     SemiAlternative --> SemiCoproduct
+ *     SemiApplicative --> SemiProduct
+ *     SemiApplicative --> Covariant
+ *     Applicative --> SemiApplicative
+ *     Chainable --> FlatMap
+ *     Chainable ---> Covariant
+ *     Monad --> FlatMap
+ *     Monad --> Pointed
+ *     Pointed --> Of
+ *     Pointed --> Covariant
+ *     Product --> SemiProduct
+ *     Product --> Of
+ *     SemiProduct --> Invariant
+ *     Covariant --> Invariant
+ *     SemiCoproduct --> Invariant
+ * ```
+ *
+ * @module
+ * @internal
+ */
+
 export type { FlatMap } from './flat-map.ts'
 export type { Invariant } from './invariant.ts'
 export { Covariant } from './covariant.ts'
