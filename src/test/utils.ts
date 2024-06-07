94cee79dbd02721a6ba1d569b478492542c7c212
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-06-07 23:11:10 +0200

    test(internal): Convert Util namespace to class in utils.ts
    The code in utils.ts has been updated to replace the existing Util
    namespace with a class. This revision includes the conversion of
    "deepStrictEqual" and "optionEqual" methods into static methods of
    the new class. Descriptive comments are added for improved
    understanding of these methods.

--- src/test/utils.ts
+++ src/test/utils.ts
@@ -1,13 +1,22 @@
 import { assertEquals, assertStrictEquals } from 'jsr:@std/assert'
 import type { Option } from '../option/option.ts'
 
-// deno-lint-ignore no-namespace
-export namespace Util {
-	export const deepStrictEqual = <A>(actual: A, expected: A) => {
+/**
+ * Utility class with helper methods for various test assertions.
+ */
+// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
+export class Util {
+	/**
+	 * Asserts that the actual value is equal to the expected value as well as their types.
+	 */
+	public static readonly deepStrictEqual = <A>(actual: A, expected: A) => {
 		assertEquals(actual, expected)
 	}
 
-	export const optionEqual = <A>(actual: Option.Type<A>, expected: Option.Type<A>) => {
+	/**
+	 * Compares two Option.Type instances for equality and asserts that they are equal.
+	 */
+	public static readonly optionEqual = <A>(actual: Option.Type<A>, expected: Option.Type<A>) => {
 		assertStrictEquals(actual.equals(expected), true)
 	}
 }
