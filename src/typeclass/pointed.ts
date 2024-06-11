abdf6337d9345c5eccc9204cc07faa1219324a39
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-06-11 22:47:36 +0200

    docs(internal): Extend `Pointed` type class interfaces
    The Pointed type class has been extended with two additional
    interfaces, Pipeable and Fluent. These interfaces extend the
    functionalities of Covariant and Of, allowing for a more fluid usage
    of type classes with Covariant and Of capabilities.

--- src/typeclass/pointed.ts
+++ src/typeclass/pointed.ts
@@ -2,7 +2,19 @@ import type { TypeLambda } from '../internal/hkt.ts'
 import type { Covariant } from './covariant.ts'
 import type { Of } from './of.ts'
 
+/**
+ * The Pointed namespace provides interfaces for working with type classes that have both Covariant and Of capabilities.
+ */
 export declare namespace Pointed {
+	/**
+	 * Pipeable interface for Pointed type class, extending the functionalities of Covariant.Pipeable and Of.Pipeable.
+	 * @template F TypeLambda
+	 */
 	export interface Pipeable<F extends TypeLambda> extends Covariant.Pipeable<F>, Of.Pipeable<F> {}
+
+	/**
+	 * Fluent interface for Pointed type class, extending the functionalities of Covariant.Fluent.
+	 * @template F TypeLambda
+	 */
 	export interface Fluent<F extends TypeLambda> extends Covariant.Fluent<F> {}
 }
